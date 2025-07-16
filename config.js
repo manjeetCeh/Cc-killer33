119058d5bc17beb754e1fb8d22553ceace9b09dc
manjeetCeh <125298895+manjeetCeh@users.noreply.github.com>
2025-07-16 13:05:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
-    telegramBotToken: "7592733993:AAEsYFdLKos9klgdyaht5SjkNTnf-pwFleM", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["aarav_0008","alen_heree"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["aarav_0008","alen_heree"] // username or id of the user who can use the bot.
+    telegramBotToken: "7713788107:AAGAOkh-m7oYqUR38ffP0oLZ-QAqPDJ0uqI", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["Xencryptic","Xencryptic"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["Xencryptic","Xencryptic"] // username or id of the user who can use the bot.
 };
